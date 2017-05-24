078b792467e7583a71d9e9482266941c0ca3725f
yamaneko <yunisy1018@gmail.com>
2017-05-24 08:49:58 +0000

    シードデータ作成

--- Gemfile
+++ Gemfile
@@ -41,7 +41,10 @@ group :development, :test do
 
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
+  gem 'pry-rails'
+  gem 'better_errors'
 end
 
 gem 'twitter-bootstrap-rails'
 gem 'devise'
+gem 'faker'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  name = Faker::Name.name
+  User.create!(email: email,
+               name: name,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
+
+n = 2
+while n <= 100
+ Topic.create(
+      content:"もうすぐ夏",
+      user_id: n
+     )
+     n = n + 1
+end
